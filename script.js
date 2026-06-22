a52c7ee7a55200b6343c837a4777619e894a48de
my-websitesolution <satriorich2011@gmail.com>
2026-06-22 14:43:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const menu = document.querySelector(".menu");
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+const tambahan = document.querySelector(".tambahan");
+
+const more = document.getElementById("more");
+
+more.addEventListener("click", () => {
+  tambahan.classList.toggle("show");
+
+  if (tambahan.classList.contains("show")) {
+    more.textContent = "Read Less";
+  } else {
+    more.textContent = "Learn More ";
+  }
+});
